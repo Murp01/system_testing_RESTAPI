869f903b6d399367777a205cebb595397dbcea6d
paul murphy <pmurphy35@hotmail.com>
2021-03-13 14:38:32 +0000

    Completed set up class and base test module

--- tests/base_test.py
+++ tests/base_test.py
@@ -12,19 +12,24 @@ from db import db
 
 
 class BaseTest(TestCase):
-    def setUp(self):
-        # Make sure database exists
+
+    @classmethod
+    def setUpClass(cls):
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
         #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/test'
         with app.app_context():
             db.init_app(app)
+
+
+    def setUp(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+        #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost:5432/test'
+        with app.app_context():
             db.create_all()
-        # Get a test client
-        self.app = app.test_client()
+        self.app = app.test_client
         self.app_context = app.app_context
 
     def tearDown(self):
-        # Database is blank
         with app.app_context():
             db.session.remove()
             db.drop_all()
